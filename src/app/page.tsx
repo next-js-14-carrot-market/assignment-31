786bc87e33979f77d03914b194b92f6c238faa97
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-07 05:13:19 +0900

    init: create project(#30기반)

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import TweetList from "@/components/tweet-list";
+import { getInitialTweets } from "../service/tweetService";
+
+export default async function MainPage() {
+  const tweets = await getInitialTweets();
+  return (
+    <div className="p-5 flex flex-col gap-5">
+      <TweetList initialTweets={tweets} />
+    </div>
+  );
+}
