a2c707e66c2ab08243ed3195b0fe3590ba3ec385
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-07 06:00:01 +0900

    feat: Assignment#31 - Create Tweet
    - 트윗을 업로드하는 폼 구현
    - 트윗 업로드 컴포넌트를 / 페이지 상단에 배치

--- src/service/tweetService.ts
+++ src/service/tweetService.ts
@@ -1,13 +1,17 @@
 "use server";
 
 import { Prisma } from "@prisma/client";
+import { z } from "zod";
+import { redirect } from "next/navigation";
 
 import db from "../utils/db";
+import { getSession } from "@/utils/session";
 
+const LIMIT_NUMBER = 2;
 export const getInitialTweets = async () => {
   const tweets = db.tweet.findMany({
     include: { user: true },
-    take: 3,
+    take: LIMIT_NUMBER,
     orderBy: {
       created_at: "desc",
     },
@@ -16,7 +20,6 @@ export const getInitialTweets = async () => {
 };
 export type InitialTweets = Prisma.PromiseReturnType<typeof getInitialTweets>;
 
-const LIMIT_NUMBER = 2;
 export async function getTweetsByPage(page: number) {
   const tweets = await db.tweet.findMany({
     include: { user: true },
@@ -36,4 +39,38 @@ export async function getPaginatedTweets(page: number) {
   const TWEETS_TOTAL_COUNT = await getTweetTotalCount();
   const isLastPage = TWEETS_TOTAL_COUNT <= LIMIT_NUMBER * page;
   return { tweets, isLastPage };
+}
+
+const tweetSchema = z.object({
+  tweet: z.string({
+    required_error: "Tweet is required.",
+  }),
+});
+
+export async function uploadTweet(_: unknown, formData: FormData) {
+  const data = {
+    tweet: formData.get("tweet"),
+  };
+
+  const result = tweetSchema.safeParse(data);
+  if (!result.success) {
+    return {
+      error: result.error.flatten(),
+      isSuccess: false,
+    };
+  }
+  const session = await getSession();
+  if (session.id) {
+    const tweet = await db.tweet.create({
+      data: {
+        tweet: result.data.tweet,
+        user: {
+          connect: {
+            id: session.id,
+          },
+        },
+      },
+    });
+    redirect(`/tweets/${tweet.id}`);
+  }
 }
